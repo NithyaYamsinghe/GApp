a01a2333a6327c2b92c6e91720c6dfb0a4d149cf
NithyaYamsinghe <nitheeromeshi@gmail.com>
2021-10-02 23:20:47 +0530

    initial ui implementation

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,15 +1,29 @@
 import React from "react";
+import { Button } from "@mui/material";
+import Box from "@material-ui/core/Box";
+import GoogleIcon from "@mui/icons-material/Google";
 
 const LoginForm = () => {
   return (
     <div>
-      <a
-        href="https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?redirect_uri=http://localhost:3000/redirect&prompt=consent&response_type=code&client_id=994172784584-j10p790442ks2msidqrseecjtu1lh622.apps.googleusercontent.com&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive&access_type=offline&flowName=GeneralOAuthFlow"
-        target="_blank"
-        rel="noreferrer"
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="100vh"
+        component="div"
       >
-        <button>Login With Google</button>
-      </a>
+        <a
+          href="https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?redirect_uri=http://localhost:3000/redirect&prompt=consent&response_type=code&client_id=994172784584-j10p790442ks2msidqrseecjtu1lh622.apps.googleusercontent.com&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fdrive&access_type=offline&flowName=GeneralOAuthFlow"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Button variant="outlined" color="error">
+            <GoogleIcon style={{ marginRight: ".6rem" }} /> LOGIN WITH GOOGLE{" "}
+          </Button>
+        </a>
+      </Box>
+
       <p>locals{localStorage.getItem("code")}</p>
     </div>
   );
--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -3,12 +3,23 @@ import { Switch, Route } from "react-router-dom";
 import LoginScreen from "./../pages/LoginScreen";
 import RedirectScreen from "./../pages/RedirectScreen";
 
+import Dashboard from "./../pages/Dashboard";
+import GDrive from "./../pages/GDrive";
+import Contacts from "./../pages/Contacts";
+import MyAccount from "./../pages/MyAccount";
+
 const AppRouter = () => {
   return (
     <React.Fragment>
       <Switch>
         <Route exact path="/" component={LoginScreen}></Route>
         <Route path="/redirect" component={RedirectScreen}></Route>
+
+        <Dashboard>
+          <Route exact path="/gdrive" component={GDrive} />
+          <Route exact path="/contacts" component={Contacts} />
+          <Route exact path="/myaccount" component={MyAccount} />
+        </Dashboard>
       </Switch>
     </React.Fragment>
   );
