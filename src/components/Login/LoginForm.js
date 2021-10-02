ef7f630a0496ce7075b9b03f12591b1ea353cf52
NithyaYamsinghe <nitheeromeshi@gmail.com>
2021-10-02 19:30:15 +0530

    complete initial oAuth connection

--- src/components/Login/LoginForm.js
+++ src/components/Login/LoginForm.js
@@ -1,7 +1,25 @@
 import React from "react";
+import GoogleLogin from "react-google-login";
+import { useAuth } from "../../context/Context";
 
 const LoginForm = () => {
-  return <div>Hello From GApp</div>;
+  // const responseGoogle = (response) => {
+  //   console.log(response);
+  //   console.log(response.profileObj);
+  // };
+  const { responseGoogle, email } = useAuth();
+  return (
+    <div>
+      <GoogleLogin
+        clientId="994172784584-j10p790442ks2msidqrseecjtu1lh622.apps.googleusercontent.com"
+        buttonText="LoginWithGoogle"
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+        cookiePolicy={"single_host_origin"}
+      />
+      <p>{email}</p>
+    </div>
+  );
 };
 
 export default LoginForm;
