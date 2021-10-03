2dabb025440717154c4e0d274c5959d648d5c6f7
yohansrimal <yohansrimalranasinghe@gmail.com>
2021-10-03 14:56:02 +0530

    Dropzone Added

---
+++ src/pages/FileUploadScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import FileUpload from "../components/FileUpload/Upload";
+
+export default function FileUploadScreen() {
+  return (
+    <div>
+      <FileUpload />
+    </div>
+  );
+}
