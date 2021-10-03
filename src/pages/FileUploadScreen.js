5d6cf15859187733f9ad117b3cae1a4b7386ba5b
Yasmika Saubhagya <it18209280@my.sliit.lk>
2021-10-03 08:56:51 -0700

    Add descriptive comments

--- src/pages/FileUploadScreen.js
+++ src/pages/FileUploadScreen.js
@@ -1,6 +1,9 @@
 import React from "react";
 import FileUpload from "../components/FileUpload/Upload";
 
+/**
+ * Common file upload component which handles file uploadings
+ */
 export default function FileUploadScreen() {
   return (
     <div>
--- src/pages/GoogleFileCommentScreen.js
+++ src/pages/GoogleFileCommentScreen.js
@@ -1,6 +1,10 @@
 import React from "react";
 import CreateCommentForm from "../components/Comments/CreateCommentForm";
 
+/**
+ * Comment Screen which provides input components
+ * to add new comment on a file
+ */
 const GoogleFileCommentScreen = ({ match }) => {
   const id = match.params.id;
   return <CreateCommentForm id={id} />;
