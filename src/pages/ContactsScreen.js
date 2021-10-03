0a3fe82172c3d4d784f8f3cc488c6a64895e5219
Yasmika Saubhagya <it18209280@my.sliit.lk>
2021-10-03 09:12:23 -0700

    Add comments

--- src/pages/ContactsScreen.js
+++ src/pages/ContactsScreen.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ContactsTable from "./../components/Contacts/ContactsTable";
-
+/**
+ * Contact Screen component to render the table which contains
+ * google contacts of the person
+ */
 const ContactsScreen = () => {
   return (
     <div>
